db5da516ba50d25362a09d090821cc37ef580ba3
Zivorad Trickovic <zivoradtrickovic@gmail.com>
2024-09-03 11:50:58 +0200

    added clientUi

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+﻿using API.Dtos;
+using Core.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("Not a good request");
+        }
+
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception("This is a test exception");
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationError(CreateProductDto product)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ clientUI/src/app/layout/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+import { MatBadge } from '@angular/material/badge';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatIcon, MatButton, MatBadge],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
